46068ce8758c73f2290c28e3af42d1a02d0507dd
Jess Graham <jess@jessgraham.dev>
2025-02-06 01:43:51 -0500

    Create CacheService for caching requests for a lite in-memory mechanism

--- src/app/api/services/cache-service.ts
+++ src/app/api/services/cache-service.ts
@@ -1,3 +1,53 @@
-export interface ICacheService {}
+import { config } from '@/lib';
 
-export class CacheService {}
+export interface CacheSlot {
+    data: any;
+    timeout: NodeJS.Timeout;
+}
+
+export interface ICacheService {
+    getCachedSlot(key: string): any | undefined;
+    setCachedSlot(key: string, data: any): void;
+}
+
+export class CacheService implements ICacheService {
+    private readonly CACHE_TTL: number = config.cache.ttl; // Controls the TTL for the cache slots
+    private readonly cache = new Map<string, CacheSlot>();
+
+    constructor() {
+        // eslint-disable-next-line no-console
+        console.log('Initializing cache for data');
+    }
+
+    public getCachedSlot<TData = any>(key: string): TData | undefined {
+        const slot = this.cache.get(key);
+
+        if (slot) {
+            // eslint-disable-next-line no-console
+            console.log('\x1b[94m%s\x1b[0m', `Refreshing cache slot: ${key}`);
+            slot.timeout.refresh();
+            return slot.data;
+        }
+
+        return undefined;
+    }
+
+    public setCachedSlot(key: string, data: any): void {
+        // eslint-disable-next-line no-console
+        console.log('\x1b[32m%s\x1b[0m', `Adding cache slot: ${key}`);
+
+        this.cache.set(key, {
+            data,
+            timeout: setTimeout(() => {
+                this.deleteCacheSlot(key);
+            }, this.CACHE_TTL),
+        });
+    }
+
+    private deleteCacheSlot(key: string) {
+        // eslint-disable-next-line no-console
+        console.log('\x1b[33m%s\x1b[0m', `Deleting cache slot: ${key}`);
+
+        this.cache.delete(key);
+    }
+}
