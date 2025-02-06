ecf1b6593c98735ebf70069237f61b75b8ebe9a9
Jess Graham <jess@jessgraham.dev>
2025-02-06 01:35:01 -0500

    Add index to advocates table for performance improvements to searching / filtering

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,24 +1,38 @@
-import { sql } from "drizzle-orm";
+import { sql } from 'drizzle-orm';
 import {
-  pgTable,
-  integer,
-  text,
-  jsonb,
-  serial,
-  timestamp,
-  bigint,
-} from "drizzle-orm/pg-core";
+    pgTable,
+    integer,
+    text,
+    jsonb,
+    serial,
+    timestamp,
+    bigint,
+    index,
+} from 'drizzle-orm/pg-core';
 
-const advocates = pgTable("advocates", {
-  id: serial("id").primaryKey(),
-  firstName: text("first_name").notNull(),
-  lastName: text("last_name").notNull(),
-  city: text("city").notNull(),
-  degree: text("degree").notNull(),
-  specialties: jsonb("payload").default([]).notNull(),
-  yearsOfExperience: integer("years_of_experience").notNull(),
-  phoneNumber: bigint("phone_number", { mode: "number" }).notNull(),
-  createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
-});
+const advocates = pgTable(
+    'advocates',
+    {
+        id: serial('id').primaryKey(),
+        firstName: text('first_name').notNull(),
+        lastName: text('last_name').notNull(),
+        city: text('city').notNull(),
+        degree: text('degree').notNull(),
+        specialties: jsonb('payload').default([]).notNull(),
+        yearsOfExperience: integer('years_of_experience').notNull(),
+        phoneNumber: bigint('phone_number', { mode: 'number' }).notNull(),
+        createdAt: timestamp('created_at').default(sql`CURRENT_TIMESTAMP`),
+    },
+    (table) => ({
+        // Adds proper index for helping with searching performance
+        advocateIndex: index('advocate_index').on(
+            table.firstName,
+            table.lastName,
+            table.city,
+            table.degree,
+            table.specialties,
+        ),
+    }),
+);
 
 export { advocates };
