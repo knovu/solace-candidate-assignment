e7327433bdb86e2e553c67d84d5ae30d17f313c8
Jess Graham <jess@jessgraham.dev>
2025-02-06 02:40:58 -0500

    Remove duplicate Table.Header component from advocate-table

--- src/app/components/advocate-table/advocate-table.tsx
+++ src/app/components/advocate-table/advocate-table.tsx
@@ -10,17 +10,15 @@ const AdvocateTable = (props: AdvocateTableProps) => {
         <VStack w="100%">
             <Table.Root>
                 <Table.Header>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.ColumnHeader>First name</Table.ColumnHeader>
-                            <Table.ColumnHeader>Last name</Table.ColumnHeader>
-                            <Table.ColumnHeader>City</Table.ColumnHeader>
-                            <Table.ColumnHeader>Degree</Table.ColumnHeader>
-                            <Table.ColumnHeader>Specialties</Table.ColumnHeader>
-                            <Table.ColumnHeader>Years of experience</Table.ColumnHeader>
-                            <Table.ColumnHeader>Phone number</Table.ColumnHeader>
-                        </Table.Row>
-                    </Table.Header>
+                    <Table.Row>
+                        <Table.ColumnHeader>First name</Table.ColumnHeader>
+                        <Table.ColumnHeader>Last name</Table.ColumnHeader>
+                        <Table.ColumnHeader>City</Table.ColumnHeader>
+                        <Table.ColumnHeader>Degree</Table.ColumnHeader>
+                        <Table.ColumnHeader>Specialties</Table.ColumnHeader>
+                        <Table.ColumnHeader>Years of experience</Table.ColumnHeader>
+                        <Table.ColumnHeader>Phone number</Table.ColumnHeader>
+                    </Table.Row>
                 </Table.Header>
             </Table.Root>
         </VStack>
