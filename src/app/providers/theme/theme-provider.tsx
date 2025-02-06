1aea87e4acc9b37419221c3a183327aec0c08d78
Jess Graham <jess@jessgraham.dev>
2025-02-06 02:23:58 -0500

    Remove tailwind and postcss libs

--- src/app/providers/theme/theme-provider.tsx
+++ src/app/providers/theme/theme-provider.tsx
@@ -3,13 +3,21 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { PropsWithChildren } from 'react';
 import system from './system';
+import { Toaster } from '@/app/components';
 
 interface ThemeProviderProps extends PropsWithChildren {}
 
 const ThemeProvider = (props: ThemeProviderProps) => {
     const { children } = props;
 
-    return <ChakraProvider value={system}>{children}</ChakraProvider>;
+    return (
+        <ChakraProvider value={system}>
+            {/* Register global components */}
+            <Toaster />
+
+            {children}
+        </ChakraProvider>
+    );
 };
 
 export default ThemeProvider;
