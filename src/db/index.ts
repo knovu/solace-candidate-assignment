f6a1b7a905300f2debfd08da99684b5047c1f94f
Jess Graham <jess@jessgraham.dev>
2025-02-06 01:33:11 -0500

    Migrate db set up to use strict types and config variables for strict bootstrapping

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,20 +1,21 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { config } from '@/lib';
+import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
 
-const setup = () => {
-  if (!process.env.DATABASE_URL) {
-    console.error("DATABASE_URL is not set");
-    return {
-      select: () => ({
-        from: () => [],
-      }),
-    };
-  }
+const setup = (): PostgresJsDatabase | undefined => {
+    if (!config.connectToDatabase) {
+        return undefined;
+    }
 
-  // for query purposes
-  const queryClient = postgres(process.env.DATABASE_URL);
-  const db = drizzle(queryClient);
-  return db;
+    // for query purposes
+    const queryClient = postgres({
+        host: config.db.host,
+        database: config.db.database,
+        user: config.db.user,
+        pass: config.db.pass,
+    });
+    const db = drizzle(queryClient);
+    return db;
 };
 
 export default setup();
